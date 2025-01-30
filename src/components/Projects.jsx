249653ebfdad514da2951fda1422600f34986fe9
Imad YAHYAOUI <imad@github.com>
2025-01-30 19:16:51 +0100

    Projects Section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import portfolio1 from "../assets/portfolio-bg1.jpg"
+import portfolio2 from "../assets/portfolio-bg2.jpg"
+import portfolio3 from "../assets/portfolio-bg3.jpg"
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+    return (
+        <section className="py-12 px-6 lg:h-[85vh] lg:mt-0 pt-20 w-full overflow-hidden flex justify-center items-center">
+            <div className="container mx-auto text-center">
+                <h3 className="text-[#128cf0] text-sm font-bold uppercase mb-2 mt-10">Projects</h3>
+                <h1 className="text-4xl font-bold mb-6">Recent Portfolios</h1>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-export default Projects
+                    <div className="relative group overflow-hidden rounded-lg shadow-lg">
+                        <img src={ portfolio1 } alt="Business Planning" className="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-300" />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-70 transition-all duration-300 flex items-end p-4">
+                            <div className="text-white flex flex-col items-start">
+                                <p className="text-sm uppercase">Shopping</p>
+                                <h3 className="text-xl font-bold">E-Commerce Excellence</h3>
+                                <Link to="#" className="mt-2 hover:inline-block text-sm font-bold underline">View Portfolio</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="relative group overflow-hidden rounded-lg shadow-lg">
+                        <img src={ portfolio2 } alt="Business Planning" className="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-300" />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-70 transition-all duration-300 flex items-end p-4">
+                            <div className="text-white flex flex-col items-start">
+                                <p className="text-sm uppercase">Analytics</p>
+                                <h3 className="text-xl font-bold">Business Insights Platform</h3>
+                                <Link to="#" className="mt-2 hover:inline-block text-sm font-bold underline">View Portfolio</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="relative group overflow-hidden rounded-lg shadow-lg">
+                        <img src={ portfolio3 } alt="Business Planning" className="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-300" />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-70 transition-all duration-300 flex items-end p-4">
+                            <div className="text-white flex flex-col items-start">
+                                <p className="text-sm uppercase">Design</p>
+                                <h3 className="text-xl font-bold">Creative Portfolio Showcase</h3>
+                                <Link to="#" className="mt-2 hover:inline-block text-sm font-bold underline">View Portfolio</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button className="mt-8 bg-[#128cf0] text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors"> View Projects </button>
+            </div>
+        </section>
+    );
+};
+
+export default Projects;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../style/Services.css';
+import { LiaLaptopCodeSolid } from 'react-icons/lia';
+import { FcStatistics } from 'react-icons/fc';
+import { FaWordpress } from 'react-icons/fa';
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+  const [expandedService, setExpandedService] = useState(null);
 
-export default Services
+  const services = [
+    {
+      id: 1,
+      icon: <LiaLaptopCodeSolid className='services_icon' />,
+      title: 'Website Building Using Programming Languages',
+      text: `Create stunning, high-performing websites tailored to your needs using cutting-edge programming languages and frameworks, ensuring functionality, scalability, and a seamless user experience.`,
+    },
+    {
+      id: 2,
+      icon: <FaWordpress className='services_icon' />,
+      title: 'WordPress Website Building',
+      text: 'Build dynamic and visually captivating websites with WordPress, leveraging its flexibility and extensive plugins to create tailored solutions for businesses of all sizes.',
+    },
+    {
+      id: 3,
+      icon: <FcStatistics className='services_icon' />,
+      title: 'Social Media Management',
+      text: 'Enhance your online presence with expert social media management, including targeted campaigns on Facebook, Instagram, and LinkedIn to boost engagement and drive results.',
+    },
+  ];
+
+  const toggleText = (id) => {
+    setExpandedService((prev) => (prev === id ? null : id));
+  };
+
+  return (
+    <div className='services'>
+      <div className='title'>
+        <h1>Our Services</h1>
+        <h3>
+          "Offering comprehensive solutions, including website development,
+          software services, and social media management to drive growth and
+          innovation for your organization."
+        </h3>
+      </div>
+      <div className='services_info_container'>
+        {services.map(({ id, icon, title, text }) => {
+          const isExpanded = expandedService === id;
+          return (
+            <div
+              className='services_info'
+              key={id}
+            >
+              {/* // hgj */}
+              {icon}
+              <h2>{title}</h2>
+              <p>{isExpanded ? text : `${text.substring(0, 100)}...`}</p>
+              <button
+                onClick={() => toggleText(id)}
+                className='view_more_button'
+              >
+                {isExpanded ? 'View Less' : 'View More'}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
